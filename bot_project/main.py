0ffc8cd8138edea6ac6137024fe76b7d5c58bbd6
majishan-dev <development0majishan@gmail.com>
2024-02-26 21:46:37 +0900

    modified env

--- bot_project/main.py
+++ bot_project/main.py
@@ -7,10 +7,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # APIキーとアクセストークンの設定
-api_key = os.getenv('api_key')
-api_key_secret= os.getenv('api_key_secret')
-access_token= os.getenv('access_token')
-access_token_secret= os.getenv('access_token_secret')
+api_key = os.getenv('API_KEY')
+api_key_secret= os.getenv('API_KEY_SECRET')
+access_token= os.getenv('ACCESS_TOKEN')
+access_token_secret= os.getenv('ACCESS_TOKEN_SECRET')
 
 client = tweepy.Client(
     consumer_key=api_key,
